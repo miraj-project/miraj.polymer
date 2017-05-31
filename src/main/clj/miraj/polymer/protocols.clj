f4442301884b8e196ead60c603ac2e28e2a1bc85
Gregg Reynolds <reynolds-gregg@norc.org>
2017-05-31 09:15:22 -0500

    add Gesture protocol
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- src/main/clj/miraj/polymer/protocols.clj
+++ src/main/clj/miraj/polymer/protocols.clj
@@ -15,3 +15,10 @@
   (attributeChanged [this])
   (ready [this]))
 
+(defprotocol ^{:co-protocol? true}
+    Gesture
+  "Polymer Gesture Events https://www.polymer-project.org/1.0/docs/devguide/gesture-events"
+  (down [evt])
+  (up [evt])
+  (tap [evt])
+  (track [evt]))
